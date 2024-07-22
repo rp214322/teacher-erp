c9bddd845f618948b9b7facbca39045e89287261
rp214322 <rp214322@gmail.com>
2024-07-22 10:20:32 +0530

    remove

--- routes/web.php
+++ routes/web.php
@@ -18,28 +18,24 @@ Auth::routes();
 Route::redirect('/', '/dashboard')->name('home');
 
 //Admin Section
-Route::get('login',[\App\Http\Controllers\Admin\AuthController::class,'getLogin'])->name('admin.login');
-Route::post('login',[\App\Http\Controllers\Admin\AuthController::class,'postLogin'])->name('admin.post.login');
+Route::get('login',[\App\Http\Controllers\AuthController::class,'getLogin'])->name('admin.login');
+Route::post('login',[\App\Http\Controllers\AuthController::class,'postLogin'])->name('admin.post.login');
 
-Route::middleware('admin')->namespace('\App\Http\Controllers\Admin')->name('admin.')->group(function(){
+Route::middleware('admin')->namespace('\App\Http\Controllers')->name('admin.')->group(function(){
 
-	Route::get('dashboard',[\App\Http\Controllers\Admin\DashboardController::class,'index'])->name('dashboard');
+	Route::get('dashboard',[\App\Http\Controllers\DashboardController::class,'index'])->name('dashboard');
 
 	// Route for profile
-	Route::get('profile',[\App\Http\Controllers\Admin\AuthController::class,'getProfile'])->name('profile');
-	Route::post('profile',[\App\Http\Controllers\Admin\AuthController::class,'postProfile'])->name('post.profile');
+	Route::get('profile',[\App\Http\Controllers\AuthController::class,'getProfile'])->name('profile');
+	Route::post('profile',[\App\Http\Controllers\AuthController::class,'postProfile'])->name('post.profile');
 
 	// Route for Change Password
-	Route::get('password',[\App\Http\Controllers\Admin\AuthController::class,'getPassword'])->name('password');
-	Route::post('password',[\App\Http\Controllers\Admin\AuthController::class,'postPassword'])->name('post.password');
-
-	// Route for Setting
-    Route::get('settings',[\App\Http\Controllers\Admin\DashboardController::class,'settings'])->name('setting');
-    Route::post('settings',[\App\Http\Controllers\Admin\DashboardController::class,'post_settings'])->name('post.setting');
+	Route::get('password',[\App\Http\Controllers\AuthController::class,'getPassword'])->name('password');
+	Route::post('password',[\App\Http\Controllers\AuthController::class,'postPassword'])->name('post.password');
 
     Route::resource('users',UsersController::class);
 
 	// Route for Logout
-	Route::post('logout',[\App\Http\Controllers\Admin\AuthController::class,'getLogout'])->name('admin.logout');
+	Route::post('logout',[\App\Http\Controllers\AuthController::class,'getLogout'])->name('admin.logout');
 
 });
